1ef4ecc9f04d90159d74608551726d1860e6daaa
Will Griffin <willhgriffin@icloud.com>
2025-04-30 17:21:51 -0400

    Updated to gfx2

--- lab11/submit/gfx.h
+++ lab11/submit/gfx.h
@@ -1,10 +1,10 @@
-// A simple graphics library for CSE 20311 
-// Last edit: April 6, 2025
+// gfx2.h
 
-#ifndef GFX_H
-#define GFX_H
+#ifndef GFX2_H
+#define GFX2_H
 
 #include <X11/Xlib.h>
+#include <X11/cursorfont.h> 
 
 // Open a new graphics window. 
 void gfx_open( int width, int height, const char *title );
@@ -46,6 +46,45 @@ void gfx_circle( int xc, int yc, int r );
 
 // Display a string at (x,y) 
 void gfx_text( int x, int y , const char *text );
+
+// -------------------
+// new functions added 
+// -------------------
+
+// Draw a filled circle centered at (xc,yc) with radius r 
+void gfx_fill_circle( int xc, int yc, int r );
+
+// Draw an ellipse centered at (xc,yc) with radii r1 and r2 
+void gfx_ellipse( int xc, int yc, int r1, int r2 );
+ 
+// Draw an arc whose top left corner of its bounding rectangle is at (x,y),
+//  with width w and height h, starting at angle a1 and sweeping an angle of a2 (degrees);
+//  a1 of 0 is at the 3 O'Clock position, and the a2 sweep is positive counter-clockwise 
+void gfx_arc( int x, int y, int w, int h, int a1, int a2 );
+
+// Draw a filled arc (similar description to gfx_arc)
+void gfx_fill_arc( int x, int y, int w, int h, int a1, int a2 );
+
+// Draw a rectangle with top-left corner at (x,y) with width w and height h 
+void gfx_rectangle( int x, int y, int w, int h );
+
+// Draw a filled rectangle (similar description to gfx_rectangle)
+void gfx_fill_rectangle( int x, int y, int w, int h );
+
+// Draw a polygon whose "num_pts" number of corners are in the "pointsarr" array
+//  (note: uses Xlib's XPoint struct) 
+void gfx_polygon( XPoint *pointsarr, int num_pts );
+
+// Draw a filled polygon (similar description to gfx_polygon)
+void gfx_fill_polygon( XPoint *pointsarr, int num_pts );
+
+// Change the font that gfx_text will use 
+//   (see the file /usr/share/X11/fonts/misc/fonts.alias for possible fonts)
+void gfx_changefont( char * );
+
+// change the cursor (mouse pointer)
+//   (see the file /usr/include/X11/cursorfont.h for possible cursors)
+void gfx_changecursor( int );
 
 #endif
 
