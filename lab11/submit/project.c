15200ddf577c1df095dcf5c3b8ef88ecc74b23bc
Will Griffin <willhgriffin@icloud.com>
2025-05-01 15:50:12 -0400

    Added comments

--- lab11/submit/project.c
+++ lab11/submit/project.c
@@ -56,20 +56,24 @@ int main() {
     play_letters[i].inside = inside_grey;
   }
   fresh_answer(answer_letters, border, inside_grey, &current_answer_letter);
+
+  // Set up the submit button
   submit.cx = WIN_WIDTH / 4 * 3;
   submit.cy = WIN_HEIGHT - 100;
   submit.rad = 30;
   submit.pad = 5;
   submit.border = border;
   submit.inside = inside_yellow;
-  
+
+  // Set up the clear button
   clear.cx = WIN_WIDTH / 4;
   clear.cy = WIN_HEIGHT - 100;
   clear.rad = 30;
   clear.pad = 5;
   clear.border = border;
   clear.inside = inside_yellow;
-  
+
+  // Set up the give up button
   give_up.cx = WIN_WIDTH / 4 - 85;
   give_up.cy = WIN_HEIGHT - 100;
   give_up.rad = 30;
@@ -79,34 +83,55 @@ int main() {
   
 
   while (running) {
+    // Clear the screen
     gfx_clear();
+    
     switch (current_screen) {
+
     case STARTING:
+      // Starting screen
+
+      // Display a rectangle
       bordered_rectangle(50, 50, WIN_WIDTH - 100, WIN_HEIGHT - 100, 10, border, inside_yellow);
 
+      // Text on the rectangle
       gfx_color(0, 0, 0);
       gfx_text(330, WIN_HEIGHT / 2, "Welcome to Spelling Bee!");
       gfx_text(327, WIN_HEIGHT / 2 + 20, "Press any key to continue");
-      
+
+      // Flush the rectangle to the screen
       gfx_flush();
+
+      // Wait for user input
       gfx_wait();
+      
+      // Start the game
       current_screen = PLAY;
       break;
     case PLAY:
+      // For each of the playable letters
       for (int i = 0; i < MAX_PUZZLE; i++) {
+	// Display their square
 	bordered_square_radius(play_letters[i]);
 	gfx_color(0, 0, 0);
 	char let[2];
 	sprintf(let, "%c", play_letters[i].letter);
+	// Display their letter
 	gfx_text(play_letters[i].cx - 2, play_letters[i].cy + 2, let);
       }
+
+      // For each of the answer squares
       for (int i = 0; i < 10; i++) {
+	// Display their square
 	bordered_square_radius(answer_letters[i]);
 	gfx_color(0, 0, 0);
 	char let[2];
 	sprintf(let, "%c", answer_letters[i].letter);
+	// Display their letter
 	gfx_text(answer_letters[i].cx - 2, answer_letters[i].cy + 2, let);
       }
+
+      // Display the submit, clear, and give up buttons along with their labels
       bordered_square_radius(submit);
       bordered_square_radius(clear);
       bordered_square_radius(give_up);
@@ -114,36 +139,59 @@ int main() {
       gfx_text(submit.cx - 17, submit.cy + 2, "Submit");
       gfx_text(clear.cx - 15, clear.cy + 2, "Clear");
       gfx_text(give_up.cx - 19, give_up.cy + 2, "Give Up");
+
+      // Display the previous output's text
       gfx_text(WIN_WIDTH / 2 - 40, 125, prev_output_msg);
-      
+
+      // Flush all of this to the display
       gfx_flush();
+
+      // Get a user event
       event = gfx_wait();
       if (event == 1) {
+	// Get the square the user clicked on
 	int clicked_square = get_clicked_square(play_letters, submit, clear, give_up);
-	if (clicked_square == -1) break;
+	if (clicked_square == -1) break; // If the user clicked anywhere but a playable square or button, ignore the click
+	// If the user clicked on a playable letter
 	if (clicked_square >= 0 && clicked_square <= 6) {
-	  if (clicked_square == 0) answer_letters[current_answer_letter].inside = inside_yellow;
+	  if (clicked_square == 0) answer_letters[current_answer_letter].inside = inside_yellow; // Set the answer square's color to yellow if the yellow letter was chosen
+	  // Set the answer square's letter to the clicked letter
 	  answer_letters[current_answer_letter].letter = play_letters[clicked_square].letter;
+	  // Increment the current letter position
 	  current_answer_letter++;
 	} else if (clicked_square == 10) {
-	  int success = check_answer(answer_letters, &current_puzzle);
+	  // The submit button was clicked
+	  int success = check_answer(answer_letters, &current_puzzle); // Checks whether the inputted word was a solution
+
 	  if (success >= 0) {
+	    // Display the correct word
 	    sprintf(prev_output_msg, "Correct: %s", current_puzzle.answers[success]);
 	  } else if (success == -1) {
+	    // Display the incorrect word message
 	    sprintf(prev_output_msg, "Incorrect word");
 	  } else if (success == -2) {
+	    // Display the already submitted message
 	    sprintf(prev_output_msg, "Already submitted");
 	  }
 
 	  if (current_puzzle.answer_count == current_puzzle.amount_found) {
+	    // If all correct answers are found, move to the next puzzle or end of the game
 	    if (current_file == MAX_FILE) current_screen = END;
 	    if (current_file < MAX_FILE) current_screen = IN_BETWEEN;
 	  }
+
+	  // Clear out the answer squares for a new word
 	  fresh_answer(answer_letters, border, inside_grey, &current_answer_letter);
 	} else if (clicked_square == 20) {
+	  // The clear button was clicked
+
+	  // Clear out the answer squares and user message for a new word
 	  fresh_answer(answer_letters, border, inside_grey, &current_answer_letter);
 	  sprintf(prev_output_msg, " ");
 	} else if (clicked_square == 30) {
+	  // The give up button was clicked
+	  
+	  // Move to the next puzzle or end
 	  if (current_file == MAX_FILE) current_screen = END;
 	  if (current_file < MAX_FILE) current_screen = IN_BETWEEN;
 	}
@@ -152,10 +200,13 @@ int main() {
       break;
 
     case IN_BETWEEN:
+      // Display the end of puzzle stats
       display_end_puzzle(current_puzzle, current_file, border, inside_yellow);
 
+      // Wait for user input
       gfx_wait();
 
+      // Open the next file
       current_file++;
       file = open_file(current_file);
       if (!file) {
@@ -166,6 +217,7 @@ int main() {
       // Read the puzzle
       current_puzzle = open_puzzle(file);
 
+      // Set up the new playable letters
       init_play_letters(current_puzzle, play_letters);
       play_letters[0].border = border;
       play_letters[0].inside = inside_yellow;
@@ -173,39 +225,47 @@ int main() {
 	play_letters[i].border = border;
 	play_letters[i].inside = inside_grey;
       }
-      
+
+      // Reset the answer letters for the new game
       fresh_answer(answer_letters, border, inside_grey, &current_answer_letter);
 
+      // Clear the user message for a new game
       strcpy(prev_output_msg, " ");
 
+      // Play the new game
       current_screen = PLAY;
       break;
 
     case END:
+      // Display the end of puzzle stats
       display_end_puzzle(current_puzzle, current_file, border, inside_yellow);
 
+      // Wait for user input
       gfx_wait();
+
+      // Stop the game and the program
       running = 0;
       break;
       
     default:
       break;
     }
-    
   }
   
-  
-  
-
   return 0;
 }
 
 
 void fresh_answer(LetterSquare answer[], Color border, Color inside_grey, int *current) {
+  // Reset the answer squares' positions and dimensions
   init_answer_letters(answer);
+
+  // Reset the answer squares' color
   for (int i = 0; i < 10; i++) {
     answer[i].border = border;
     answer[i].inside = inside_grey;
   }
+
+  // Set the current square postition back to 0
   *current = 0;
 }
--- lab11/submit/projectfunc.c
+++ lab11/submit/projectfunc.c
@@ -6,39 +6,51 @@
 #include "projectfunc.h"
 
 void bordered_rectangle(int x, int y, int w, int h, int pad, Color border, Color inside) {
+  // Create the bigger border rectangle with the border color
   gfx_color(border.r, border.g, border.b);
   gfx_fill_rectangle(x, y, w, h);
 
+  // Create the smaller inner rectangle with the inner color
   gfx_color(inside.r, inside.g, inside.b);
   gfx_fill_rectangle(x + pad, y + pad, w - (2 * pad), h - (2 * pad));
 }
 
 void bordered_square_radius(LetterSquare s) {
+  // Create a bordered rectangle given the LetterSquare's dimensions and colors
   bordered_rectangle(s.cx - s.rad, s.cy - s.rad, 2 * s.rad, 2 * s.rad, s.pad, s.border, s.inside);
 }
 
-    FILE* open_file(int current_file) {
-    char filename[20];
-    sprintf(filename, "word_files/%d.word", current_file);
-    FILE *fp = fopen(filename, "r");
+FILE* open_file(int current_file) {
+  char filename[20];
+  // Get the filename from the current file number
+  sprintf(filename, "word_files/%d.word", current_file);
 
-    return fp;
-  }
+  // Open and return the file pointer to the file
+  FILE *fp = fopen(filename, "r");
+
+  return fp;
+}
 
 Puzzle open_puzzle(FILE* file) {
   Puzzle output;
 
+  // Get the yellow letter and the rest of the play letters from the file
   fscanf(file, "%c", &output.central);
   fscanf(file, "%s", output.letters);
   
   int count = 0;
   while (1) {
+    // Get the word
     fscanf(file, "%s", output.answers[count]);
 
+    // Increment the count
     count++;
 
+    // Detect end of file
     if (feof(file)) break;
   }
+
+  // Set the answer count and amount found to their corresponding amounts
   output.answer_count = count;
   output.amount_found = 0;
 
@@ -47,24 +59,25 @@ Puzzle open_puzzle(FILE* file) {
 
 void init_play_letters(Puzzle puzzle, LetterSquare play_letters[]) {
   // Middle square
-  play_letters[0].letter = puzzle.central;
-  play_letters[0].cx = WIN_WIDTH / 2;
-  play_letters[0].cy = WIN_HEIGHT / 2 + 20;
-  play_letters[0].rad = 20;
-  play_letters[0].pad = 5;
+  play_letters[0].letter = puzzle.central; // Set letter
+  play_letters[0].cx = WIN_WIDTH / 2; // Set x position
+  play_letters[0].cy = WIN_HEIGHT / 2 + 20; // Set y position
+  play_letters[0].rad = 20; // Set square radius
+  play_letters[0].pad = 5; // Set border padding
 
   // External squares
   for (int i = 0; i < MAX_PUZZLE - 1; i++) {
-    play_letters[i + 1].letter = puzzle.letters[i];
-    play_letters[i + 1].cx = (WIN_WIDTH / 2) + 75 * cos(M_PI / 3 * i);
-    play_letters[i + 1].cy = (WIN_HEIGHT / 2 + 20) + 75 * sin(M_PI / 3 * i);
-    play_letters[i + 1].rad = 20;
-    play_letters[i + 1].pad = 5;
+    play_letters[i + 1].letter = puzzle.letters[i]; // Set letter
+    play_letters[i + 1].cx = (WIN_WIDTH / 2) + 75 * cos(M_PI / 3 * i); // Set x position
+    play_letters[i + 1].cy = (WIN_HEIGHT / 2 + 20) + 75 * sin(M_PI / 3 * i); // Set y position
+    play_letters[i + 1].rad = 20; // Set square radius
+    play_letters[i + 1].pad = 5; // Set border padding
   }
     
 }
 
 void init_answer_letters(LetterSquare answer_letters[]) {
+  // Sets letter, x and y positions, square radius, and border padding for all answer squares
   for (int i = 0; i < 10; i++) {
     answer_letters[i].letter = ' ';
     answer_letters[i].cx = 180 + 50 * i;
@@ -75,8 +88,12 @@ void init_answer_letters(LetterSquare answer_letters[]) {
 }
 
 int get_clicked_square(LetterSquare play_letters[], LetterSquare submit, LetterSquare clear, LetterSquare give_up) {
+  // Gets the mouse coordinates
   int xpos = gfx_xpos();
   int ypos = gfx_ypos();
+
+  // For each button (each playable letter, submit, clear, give up), the mouse position must be between the x and y boundaries of the button for it to count as a click. Each button has a different return code
+  
   for (int i = 0; i < MAX_PUZZLE; i++) {
     if (xpos >= play_letters[i].cx - play_letters[i].rad && xpos <= play_letters[i].cx + play_letters[i].rad) {
       if (ypos >= play_letters[i].cy - play_letters[i].rad && ypos <= play_letters[i].cy + play_letters[i].rad) {
@@ -99,12 +116,15 @@ int get_clicked_square(LetterSquare play_letters[], LetterSquare submit, LetterS
       return 30;
     }
   }
+
+  // If no button is clicked, return -1
   return -1;
 }
 
 int check_answer(LetterSquare answer_letters[], Puzzle *puzzle) {
   char word[11];
   for (int i = 0; i < 10; i++) {
+    // Combine all the answer squares to form the submitted word
     if (answer_letters[i].letter == ' ') {
       word[i] = '\0';
       break;
@@ -112,38 +132,50 @@ int check_answer(LetterSquare answer_letters[], Puzzle *puzzle) {
     word[i] = answer_letters[i].letter;
   }
 
+  
   for (int i = 0; i < puzzle->answer_count; i++) {
+    // Check the submitted word against all answers for the puzzle
     if (!strcmp(puzzle->answers[i], word)) {
+      // If there is a match, make sure the answer was not already submitted
       for (int j = 0; j < puzzle->amount_found; j++) {
 	if (puzzle->already_found[j] == i) return -2;
       }
+
+      // If not already submitted, add to the submission list
       puzzle->already_found[puzzle->amount_found] = i;
       puzzle->amount_found++;
       return i;
     }
   }
+
+  // If not an answer, return -1
   return -1;
 }
 
 void display_end_puzzle(Puzzle current_puzzle, int current_file, Color border, Color inside) {
+  // Display a rectangle
   bordered_rectangle(50, 50, WIN_WIDTH - 100, WIN_HEIGHT - 100, 10, border, inside);
 
   char results_text_puzzle[100];
   char results_text_1[100];
   char results_text_2[100];
-  float perc = (float) current_puzzle.amount_found / (float) current_puzzle.answer_count * 100;
+  float perc = (float) current_puzzle.amount_found / (float) current_puzzle.answer_count * 100; // Calculate % completion of the puzzle
 
+  // Set up the text to display to the user
   sprintf(results_text_puzzle, "Puzzle %d/%d", current_file, MAX_FILE);
   sprintf(results_text_1, "You correctly found %d/%d words!", current_puzzle.amount_found, current_puzzle.answer_count);
   sprintf(results_text_2, "You solved %.2f%% of the puzzle", perc);
-      
+
+  // Print the results text
   gfx_color(0, 0, 0);
   gfx_text(385, WIN_HEIGHT / 2 - 20, results_text_puzzle);
   gfx_text(317, WIN_HEIGHT / 2, results_text_1);
   gfx_text(320, WIN_HEIGHT / 2 + 20, results_text_2);
   if (current_file != MAX_FILE) {
+    // Show this to move onto the next puzzle (if one remains)
     gfx_text(335, WIN_HEIGHT / 2 + 40, "Press any key to continue");    
   } else {
+    // Show this to quit the program (if no more puzzles remain)
     gfx_text(350, WIN_HEIGHT / 2 + 40, "Press any key to quit");    
   }
 }
